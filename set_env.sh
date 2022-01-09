d9269f92c278fa21c4357681a336a95739a0d3a3
mohamed <mohamed_ben_salem@outlook.com>
2022-01-09 15:40:54 -0500

    initial commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udacitymiovision
+export POSTGRES_PASSWORD=udacitymiovision
+export POSTGRES_HOST=udacitymiovision.cuirguz2ow6i.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udacitymiovision
+export AWS_BUCKET=arn:aws:s3:::udaconnect
+export AWS_REGION=us-west-2
+export AWS_PROFILE=test
+export JWT_SECRET=testing
+export URL=http://localhost:8100
