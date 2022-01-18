f0c59562170ad336e10076241be36b3257470a1c
mohamed <mohamed_ben_salem@outlook.com>
2022-01-18 19:39:54 +0100

    fix config

--- udagram-api-feed/src/config/config.ts
+++ udagram-api-feed/src/config/config.ts
@@ -1,4 +1,4 @@
-require('dotenv').config();
+
 export const config = {
   'username': process.env.POSTGRES_USERNAME,
   'password': process.env.POSTGRES_PASSWORD,
@@ -7,7 +7,7 @@ export const config = {
   'dialect': 'postgres',
   'aws_region': process.env.AWS_REGION,
   'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
+  'aws_media_bucket': process.env.AWS_MEDIA_BUCKET,
   'url': process.env.URL,
   "dialectOptions":{
     "ssl": {
